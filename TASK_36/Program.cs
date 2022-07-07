3ca307805a132ed567ce78421f631491dd9fa72e
Nataliaada <106996389+Nataliaada@users.noreply.github.com>
2022-07-07 09:34:47 +0300

    Добавлено новое ДЗ

---
+++ TASK_36/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GetArrrayRnd(int size)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+    int index = 0;
+
+    while (index < array.Length)
+    {
+        array[index] = rnd.Next(0, 100);
+        Console.Write(array[index] + " ");
+        index++;
+
+    }
+    return array;
+}
+
+int FindSumNotEvenPosition(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i = i + 2)
+    {
+        sum = sum + arr[i];
+    }
+    return sum;
+
+}
+
+int[] array1 = GetArrrayRnd(5);
+int sum1 = FindSumNotEvenPosition(array1);
+Console.WriteLine();
+Console.WriteLine(sum1);
---
+++ TASK_38/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double[] array = {3.2, 7.5, 22.0, 2.5, 78.3};
+
+double FindMaxMin(double[] arr)
+
+{
+ double max = arr[0];
+ double min = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+        if (arr[i] < min)
+            min = arr[i];
+           
+    }
+    return max - min;
+}
+
+
+Console.WriteLine(FindMaxMin(array));
